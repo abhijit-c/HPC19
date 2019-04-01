6e0b6d5bf244ad9a11af5d159883142089dee64e
Abhi Chowdhary <ac6361@login-2-1.local>
2019-04-01 15:40:13 -0400

    Some work

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -26,7 +26,8 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
   // TODO: implement multi-threaded OpenMP scan
-  int num_threads = _omp_thread_count();
+  //int num_threads = _omp_thread_count();
+  int num_threads = 48;
   long corrector [num_threads];
   if (n < num_threads) { scan_seq(prefix_sum, A, n); }
   
