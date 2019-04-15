178861dc39da8887348f92c3481fde295721bbc1
abhijit-c <abhijit9331@gmail.com>
2019-04-15 16:22:06 -0400

    Removed slurm folder, starting to create jobs for each procedure.

--- hw4/job_mvec_mul.sh
+++ hw4/job_mvec_mul.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 #SBATCH --verbose
-#SBATCH --job-name=ac6361_poisson
-#SBATCH --output=slurm_%j.out
-#SBATCH --error=slurm_%j.err
+#SBATCH --job-name=ac6361_mvec_mul
+#SBATCH --output=mvec_mul_%j.out
+#SBATCH --err=mvec_mul_%j.err
 #SBATCH --time=00:01:00
 #SBATCH --nodes=1
 #SBATCH --mem=4GB
@@ -13,4 +13,4 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ac6361@nyu.edu
  
-srun ./poisson_jacobi
+srun ./mvec_mul
