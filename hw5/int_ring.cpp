a139277fc4270c92718999b99fc5047c727ea75c
TrostAft <abhijit9331@gmail.com>
2019-04-29 21:30:42 -0400

    ...

--- hw5/int_ring.cpp
+++ hw5/int_ring.cpp
@@ -6,35 +6,37 @@ int main(int argc, char** argv)
 {
   //MPI Setup
   MPI_Init(&argc, &argv);
-
   int mpirank, mpisize;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+  MPI_Request request;
 
   //Ring: Recieve value from mpirank-1, add yourself, send to mpirank+1 
-  int v;
-  if (mpirank == 0)
-  {
-    v = 0;
-  }
-  else
-  {
+  double tt = MPI_Wtime();
+  int v = 0;
+  if (mpirank != 0)
+  { //Recieve and add yourself
     MPI_Recv(&v, 1, MPI_INT, mpirank - 1, mpirank, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
     v += mpirank;
   }
-  MPI_Send(&v, 1, MPI_INT, 
+  // Send your value to next guy in ring
+  MPI_Isend(&v, 1, MPI_INT, 
            (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
-           MPI_COMM_WORLD);
+           MPI_COMM_WORLD, &request);
 
   if (mpirank == 0)
-  {
+  { //Once we've come full circle, output answer.
     MPI_Recv(&v, 1, MPI_INT, mpisize - 1, mpirank, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
-    printf("Zero recieved %d, and it should have been %d\n",
-           v, mpisize*(mpisize-1)/2);
+    tt = MPI_Wtime() - tt;
+    printf("Recieved %d, Truth = %d. Compute time = %f.\n",
+           v, mpisize*(mpisize-1)/2, tt );
+    printf("Latency: %e\n",
+           mpisize*tt / 1e9 );
   }
 
+  //Exit workers, algorithm done.
   MPI_Finalize();
   return 0;
 }
