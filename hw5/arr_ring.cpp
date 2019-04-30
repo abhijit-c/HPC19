9f0c96730b95ed81aa865e8951fd788f6d2fd89b
TrostAft <abhijit9331@gmail.com>
2019-04-29 21:41:47 -0400

    ?

--- hw5/arr_ring.cpp
+++ hw5/arr_ring.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv)
   int mpirank, mpisize;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+  MPI_Request request;
 
   //Ring: Recieve value from mpirank-1, add yourself, send to mpirank+1 
   double tt = MPI_Wtime();
@@ -20,6 +21,11 @@ int main(int argc, char** argv)
     v += mpirank;
   }
   // Send your value to next guy in ring
+  /*
+  MPI_Isend(&v, 1, MPI_INT, 
+           (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
+           MPI_COMM_WORLD, &request);
+  */
   MPI_Send(&v, 1, MPI_INT, 
            (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
            MPI_COMM_WORLD);
@@ -29,9 +35,9 @@ int main(int argc, char** argv)
     MPI_Recv(&v, 1, MPI_INT, mpisize - 1, mpirank, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
     tt = MPI_Wtime() - tt;
-    printf("Recieved %d, Truth = %d. Compute time = %f s.\n",
+    printf("Recieved %d, Truth = %d. Compute time = %f.\n",
            v, mpisize*(mpisize-1)/2, tt );
-    printf("Latency: %f ms\n",
+    printf("Latency: %e ms\n",
            1000*tt / mpisize );
   }
 
