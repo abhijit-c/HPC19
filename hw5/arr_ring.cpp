a6b015c46c5ce48b777047547c24fb109b83d978
abhijit-c <abhijit9331@gmail.com>
2019-04-29 22:29:51 -0400

    Done, it all works.

--- hw5/arr_ring.cpp
+++ hw5/arr_ring.cpp
@@ -9,39 +9,40 @@ int main(int argc, char** argv)
   int mpirank, mpisize;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
-  MPI_Request request;
 
-  //Ring: Recieve value from mpirank-1, add yourself, send to mpirank+1 
-  double tt = MPI_Wtime();
-  int v = 0;
-  if (mpirank != 0)
-  { //Recieve and add yourself
-    MPI_Recv(&v, 1, MPI_INT, mpirank - 1, mpirank, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
-    v += mpirank;
+  int Nrepeat = 1000; int numel = (1 << 19); double ttavg = 0.0;
+  for (int k = 0; k < Nrepeat; k++)
+  {
+    //Ring: Recieve value from mpirank-1, add yourself, send to mpirank+1 
+    int *arr = (int *)malloc( numel * sizeof(int) ); 
+    for (int i = 0; i < numel; i++) { arr[i] = 0; }
+    MPI_Barrier(MPI_COMM_WORLD);
+    double tt = MPI_Wtime();
+    if (mpirank != 0)
+    { //Recieve and add yourself
+      MPI_Recv(arr, numel, MPI_INT, mpirank - 1, mpirank, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      for (int i = 0; i < numel; i++) { arr[i] += mpirank; }
+    }
+    // Send your value to next guy in ring
+    MPI_Send(arr, numel, MPI_INT, 
+             (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
+             MPI_COMM_WORLD);
+    if (mpirank == 0)
+    { //Once we've come full circle, output answer.
+      MPI_Recv(arr, numel, MPI_INT, mpisize - 1, mpirank, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      ttavg += MPI_Wtime() - tt;
+    }
+    free(arr);
+    MPI_Barrier(MPI_COMM_WORLD);
   }
-  // Send your value to next guy in ring
-  /*
-  MPI_Isend(&v, 1, MPI_INT, 
-           (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
-           MPI_COMM_WORLD, &request);
-  */
-  MPI_Send(&v, 1, MPI_INT, 
-           (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
-           MPI_COMM_WORLD);
-
   if (mpirank == 0)
-  { //Once we've come full circle, output answer.
-    MPI_Recv(&v, 1, MPI_INT, mpisize - 1, mpirank, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
-    tt = MPI_Wtime() - tt;
-    printf("Recieved %d, Truth = %d. Compute time = %f.\n",
-           v, mpisize*(mpisize-1)/2, tt );
-    printf("Latency: %e ms\n",
-           1000*tt / mpisize );
+  {
+    printf("Bandwidth: %f Gbytes/s\n", 
+           Nrepeat*mpisize*sizeof(int)*numel / ttavg / 1e9);
   }
-
-  //Exit workers, algorithm done.
+  //Exit workers, ring done.
   MPI_Finalize();
   return 0;
 }
--- hw5/int_ring.cpp
+++ hw5/int_ring.cpp
@@ -10,32 +10,35 @@ int main(int argc, char** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
 
-  //Ring: Recieve value from mpirank-1, add yourself, send to mpirank+1 
-  double tt = MPI_Wtime();
-  int v = 0;
-  if (mpirank != 0)
-  { //Recieve and add yourself
-    MPI_Recv(&v, 1, MPI_INT, mpirank - 1, mpirank, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
-    v += mpirank;
+  int Nrepeat = 10000; double ttavg = 0.0;
+  for (int k = 0; k < Nrepeat; k++)
+  {
+    //Ring: Recieve value from mpirank-1, add yourself, send to mpirank+1 
+    double tt = MPI_Wtime();
+    int v = 0;
+    if (mpirank != 0)
+    { //Recieve and add yourself
+      MPI_Recv(&v, 1, MPI_INT, mpirank - 1, mpirank, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      v += mpirank;
+    }
+    // Send your value to next guy in ring
+    MPI_Send(&v, 1, MPI_INT, 
+             (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
+             MPI_COMM_WORLD);
+    if (mpirank == 0)
+    { //Once we've come full circle, output answer.
+      MPI_Recv(&v, 1, MPI_INT, mpisize - 1, mpirank, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      ttavg += MPI_Wtime() - tt;
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
   }
-  // Send your value to next guy in ring
-  MPI_Send(&v, 1, MPI_INT, 
-           (mpirank+1)%mpisize, (mpirank+1)%mpisize, 
-           MPI_COMM_WORLD);
-
   if (mpirank == 0)
-  { //Once we've come full circle, output answer.
-    MPI_Recv(&v, 1, MPI_INT, mpisize - 1, mpirank, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
-    tt = MPI_Wtime() - tt;
-    printf("Recieved %d, Truth = %d. Compute time = %f s.\n",
-           v, mpisize*(mpisize-1)/2, tt );
-    printf("Latency: %f ms\n",
-           1000*tt / mpisize );
+  {
+    printf("Latency: %f ms\n", 1000*ttavg / mpisize / Nrepeat );
   }
-
-  //Exit workers, algorithm done.
+  //Exit workers, ring done.
   MPI_Finalize();
   return 0;
 }
